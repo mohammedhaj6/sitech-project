4d03124e2505667d88886975985ee6e338cf7cc0
ETO <mohammedalhajmohammed6@gmail.com>
2025-11-10 09:05:36 +0200

    Save local changes before pulling

--- final-project/src/test/java/com/saucedemo/pages/MainPage.java
+++ final-project/src/test/java/com/saucedemo/pages/MainPage.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class MainPage extends BasePage {
@@ -34,6 +38,7 @@ public class MainPage extends BasePage {
 
     @Step
     public boolean isShoppingCartBadgeNumberDisplayed() {
-        return find(shoppingCartBadgeNumber).isDisplayed();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(shoppingCartBadgeNumber)).isDisplayed();
     }
 }
